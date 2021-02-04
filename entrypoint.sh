f1100e0ca03ddf0d3ec42a7fe4b1d8bddec6cb9e
Justin <68868873+jonstine@users.noreply.github.com>
2021-02-04 16:34:17 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [[ $(autopep8 --aggressive --max-line-length=116 --exclude=**/*/migrations/*.py,**/*/node_modules/**/*.py -r --diff /github/workspace/| wc -c) -ne 0 ]]; then
     echo "autopep8 needs to run"
+    autopep8 --aggressive --max-line-length=116 --exclude=**/*/migrations/*.py,**/*/node_modules/**/*.py -r --diff /github/workspace/
     exit 1
 else
     echo "autopep8 did not find formatting errors"
